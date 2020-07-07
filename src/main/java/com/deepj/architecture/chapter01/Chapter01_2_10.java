089769b4b9b7b22489e2f23a0e5190aa8dc0da6b
QingDong Zhang <qingdong.zhang@ronhan.com>
2020-07-07 15:18:30 +0800

    自定义增强for循环

--- src/main/java/com/deepj/architecture/chapter01/Chapter01_2_10.java
+++ src/main/java/com/deepj/architecture/chapter01/Chapter01_2_10.java
@@ -12,22 +12,27 @@ import java.util.*;
 public class Chapter01_2_10 {
 
     public static void main(String[] args) {
-        Integer[] arr = {1, 2, 3, 123, 5, 6, 7, 897};
-        commonCycle(arr);
+//        Integer[] arr = {1, 2, 3, 123, 5, 6, 7, 897};
+//        commonCycle(arr);
+//
+//        System.out.println("\n====================");
+//
+//        List<Integer> intArr = Arrays.asList(arr);
+//        iteratorCycle(intArr);
+//
+//        System.out.println("\n\n=========增强型for循环===========\n");
+//
+//        for (Integer ii : arr) {
+//            System.out.print(ii+",");
+//        }
+//        System.out.println("\n====================");
+//        for (Integer jj : intArr) {
+//            System.out.print(jj+",");
+//        }
 
-        System.out.println("\n====================");
-
-        List<Integer> intArr = Arrays.asList(arr);
-        iteratorCycle(intArr);
-
-        System.out.println("\n\n=========增强型for循环===========\n");
-
-        for (Integer ii : arr) {
-            System.out.print(ii+",");
-        }
-        System.out.println("\n====================");
-        for (Integer jj : intArr) {
-            System.out.print(jj+",");
+        PrintPI printPI = new PrintPI();
+        for (char x : printPI) {
+            System.out.print(x);
         }
 
     }
@@ -47,14 +52,35 @@ public class Chapter01_2_10 {
             System.out.print(next+",");
         }
     }
-
+    // 增强for 易错点
     private static void errorWhenForEnhance() {
 
         List<Integer> intArr = null;
         for (Integer ii : intArr) {
             System.out.println(ii);
         }
-
     }
 
+}
+class PrintPI implements Iterable<Character>{
+
+    public final String PIStr = String.valueOf(Math.PI);
+    public int cur = 0;
+    public int len = PIStr.length();
+
+    @Override
+    public Iterator<Character> iterator() {
+
+        return new Iterator<Character>() {
+            @Override
+            public boolean hasNext() {
+                return cur < len;
+            }
+
+            @Override
+            public Character next() {
+                return PIStr.charAt(cur++);
+            }
+        };
+    }
 }
---
+++ src/main/java/com/deepj/architecture/chapter01/Chapter01_2_11.java
@@ -0,0 +1,18 @@
+package com.deepj.architecture.chapter01;
+
+/**
+ * try-catch-resource
+ *
+ * @author qingdong.zhang
+ * @version 1.0
+ * @since 2020-07-07 14:58
+ */
+public class Chapter01_2_11 {
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
