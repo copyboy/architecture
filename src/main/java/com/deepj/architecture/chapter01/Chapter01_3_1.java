9c2824e04dd92c9449c3d4064e84da076a1bbaf3
QingDong Zhang <qingdong.zhang@ronhan.com>
2020-07-10 10:44:24 +0800

    java 基础,包装类

--- src/main/java/com/deepj/architecture/chapter01/Chapter01_3_1.java
+++ src/main/java/com/deepj/architecture/chapter01/Chapter01_3_1.java
@@ -15,6 +15,9 @@ public class Chapter01_3_1 {
 //        System.out.println(forDigitTest(11, 36));
 //        otherJudge();
 //        byteTest();
+//        integerTest();
+//        intTest2();
+        intTest3();
     }
 
 
@@ -62,7 +65,45 @@ public class Chapter01_3_1 {
     }
 
     // =========================== Integer 测试 ==============================
-    private static void intReverse() {
-//        Integer.reverse();
+    private static void integerTest() {
+        int a = 0x12345678;
+        System.out.println(Integer.toBinaryString(a));
+        System.out.println(a);
+
+        int b = Integer.reverse(a);
+        System.out.println(Integer.toBinaryString(b));
+        System.out.println(Integer.toHexString(b));
+        System.out.println(b);
+
+        int c = Integer.reverseBytes(a);
+        System.out.println(Integer.toBinaryString(c));
+        System.out.println(Integer.toHexString(c));
+
+//        0001_0010_0011_0100_0101_0110_0111_1000
+//        0001_1110_0110_1010_0010_1100_0100_1000
+//        0111_1000_0101_0110_0011_0100_0001_0010
+    }
+
+    // Integer 类型前面多少个0，后面多少个0
+    private static void intTest2() {
+        System.out.println(Integer.numberOfLeadingZeros(0xf0445566)); // 推断为0
+        System.out.println(Integer.numberOfLeadingZeros(0x1f445566)); // 推断为3
+
+        System.out.println(Integer.numberOfTrailingZeros(0x00ff1241)); // 推断为0
+        System.out.println(Integer.numberOfTrailingZeros(0x00ff1244)); // 推断为2
+        /**
+         * 结果
+         * 0
+         * 3
+         * 0
+         * 2
+         */
+    }
+
+    private static void intTest3() {
+
+        System.out.println(Integer.bitCount(0x1234));
+        System.out.println(Integer.toBinaryString(0x1234));
+
     }
 }
